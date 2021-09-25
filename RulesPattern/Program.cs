2e479fc39fb292fad22a417418e9f62e8e074d33
ramzi <ramzi@LAPTOP-MVODADDD>
2021-09-25 16:38:14 +0300

    add patterns examples

---
+++ RulesPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RulesPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
